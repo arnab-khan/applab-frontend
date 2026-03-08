463114b6b87c13c1eb847941eebffde11bbc771f
Arnab Khan <aranabkhan@gmail.com>
2026-03-08 17:23:32 +0530

    edit profile UI draft

--- src/app/features/profile/pages/edit-profile/edit-profile.ts
+++ src/app/features/profile/pages/edit-profile/edit-profile.ts
@@ -1,11 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Auth } from '../../../../core/services/auth';
+import { Thumbnail } from '../../../../shared/components/media/thumbnail/thumbnail';
+import { FormFieldsComponent } from '../../../../shared/components/forms/form-fields/form-fields';
+import { SanitizeInput } from '../../../../shared/directives/sanitize-input';
+import { AutoResizeTextarea } from '../../../../shared/directives/auto-resize';
+import { commonFormValidator } from '../../../../shared/validators/common-form-validator';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterModule,
+    FontAwesomeModule,
+    Thumbnail,
+    FormFieldsComponent,
+    SanitizeInput,
+    AutoResizeTextarea,
+  ],
   templateUrl: './edit-profile.html',
   styleUrl: './edit-profile.scss',
 })
-export class EditProfile {
+export class EditProfile implements OnInit {
+  private authService = inject(Auth);
+  private formBuilder = inject(NonNullableFormBuilder);
 
+  authState = this.authService.authState;
+  profileState = this.authService.profileState;
+  faArrowLeft = faArrowLeft;
+
+  hasClickedBasicSubmit = signal(false);
+  hasClickedCredentialsSubmit = signal(false);
+
+  basicForm!: FormGroup<{
+    name: FormControl<string>;
+    bio: FormControl<string>;
+  }>;
+
+  credentialsForm!: FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+    confirmPassword: FormControl<string>;
+    currentPassword: FormControl<string>;
+  }>;
+
+  ngOnInit(): void {
+    this.createForms();
+  }
+
+  createForms() {
+    const user = this.authState().user;
+
+    this.basicForm = this.formBuilder.group({
+      name: [user?.name || '', [commonFormValidator({
+        required: true,
+        disallowNumber: true,
+        requireLetter: true,
+        disallowSpecialChars: true,
+        maxLength: 30,
+      })]],
+      bio: ['', [commonFormValidator({ maxLength: 80 })]],
+    });
+
+    this.credentialsForm = this.formBuilder.group({
+      username: [user?.username || '', [commonFormValidator({
+        required: true,
+        disallowSpaces: true,
+        disallowSpecialChars: true,
+        minLength: 3,
+        maxLength: 20,
+      })]],
+      password: ['', [commonFormValidator({
+        requireNumber: true,
+        requireLetter: true,
+        disallowSpaces: true,
+        minLength: 6,
+        maxLength: 20,
+      })]],
+      confirmPassword: ['', [commonFormValidator({
+        disallowSpaces: true,
+        minLength: 6,
+        maxLength: 20,
+      })]],
+      currentPassword: ['', [commonFormValidator({
+        disallowSpaces: true,
+        minLength: 6,
+        maxLength: 20,
+      })]],
+    });
+  }
+
+
+  onBasicSubmit() {
+    this.hasClickedBasicSubmit.set(true);
+    console.log('Basic form value:', this.basicForm.value);
+    if (this.basicForm.valid) {
+
+    }
+  }
+
+  onCredentialsSubmit() {
+    this.hasClickedCredentialsSubmit.set(true);
+    console.log('Credentials form value:', this.credentialsForm.value);
+  }
 }
