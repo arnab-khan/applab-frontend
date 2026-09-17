7316b1b520048b9778e5605078e2d6244496f95a
Arnab Khan <aranabkhan@gmail.com>
2026-09-17 12:48:49 +0530

    remove unnecessary folders

--- .angular/telemetry-ai-build/browser/chunk-JVJFHH6Q.js
+++
@@ -1,9 +0,0 @@
-// src/app/shared/utils/link.ts
-function userProfileLink(username) {
-  return ["/user", username];
-}
-
-export {
-  userProfileLink
-};
-//# sourceMappingURL=chunk-JVJFHH6Q.js.map
